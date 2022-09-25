c33fb8e4e13278da0954036b25d8a95c7f53a6ee
sjoedwards <samuel.j.o.edwards@gmail.com>
2022-09-25 21:09:49 +0100

    feat(green): tested correct path being called

--- src/services/AxiosHttpClient/AxiosHttpClient.ts
+++ src/services/AxiosHttpClient/AxiosHttpClient.ts
@@ -1,11 +1,15 @@
+import axios from "axios";
 import { ApplicationConfigService, HttpClient } from "../../../types/internal";
 
 class AxiosHttpClient implements HttpClient {
   private apiKey;
+  private baseUrl;
   constructor(configService: ApplicationConfigService) {
     this.apiKey = "";
+    this.baseUrl = configService.get("baseUrl");
   }
-  get<T>(path: string) {
+  async get<T>(path: string) {
+    const response = await axios.get<T>(`${this.baseUrl}${path}`);
     return undefined as T;
   }
 
