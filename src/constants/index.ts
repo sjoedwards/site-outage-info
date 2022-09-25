f7d1397e6c11086bf6b26add8c89bf48b59426b8
sjoedwards <samuel.j.o.edwards@gmail.com>
2022-09-25 21:21:46 +0100

    feat(red): testing sending api key

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const COMMON_CONFIG = {
+  HEADERS: {
+    X_API_KEY: `x-api-key`,
+  },
+};
--- src/services/AxiosHttpClient/AxiosHttpClient.test.ts
+++ src/services/AxiosHttpClient/AxiosHttpClient.test.ts
@@ -1,6 +1,7 @@
 import ConfigService from "../Config/ConfigService";
 import AxiosHttpClient from "./AxiosHttpClient";
 import axios from "axios";
+import { COMMON_CONFIG } from "../../constants";
 
 jest.mock("axios");
 
@@ -22,18 +23,25 @@ describe("AxiosHttpClient Class - unit tests", () => {
     jest.restoreAllMocks;
   });
 
-  it("Calls the correct url when getting a resource", async () => {
+  it("Get: Calls the correct url when getting a resource", async () => {
     await axiosHttpClient.get(testPath);
     expect(axios.get).toHaveBeenCalledWith(`${baseUrl}${testPath}`);
   });
 
-  it("Returns the correct response when getting a resource", async () => {
+  it("Get: Returns the correct response when getting a resource", async () => {
     const testResponse = { response: "test" };
     (axios.get as jest.Mock).mockResolvedValueOnce({
       data: testResponse,
     });
     const response = await axiosHttpClient.get<{ response: string }>(testPath);
     expect(response).toEqual(testResponse);
+  });
+
+  it("Get: Sends the configured api key by default", async () => {
+    await axiosHttpClient.get<{ response: string }>(testPath);
+    expect(axios.get).toHaveBeenCalledWith(`${baseUrl}${testPath}`, {
+      headers: { [COMMON_CONFIG.HEADERS.X_API_KEY]: apiKey },
+    });
   });
 
   // TODO auth
